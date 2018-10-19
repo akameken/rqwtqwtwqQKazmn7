c3ac00a6fae34332fce0cb793e0183f06ef905d5
dldadnasjndfsjafajnsdfadnfadfkoafasdklk <43988221+dldadnasjndfsjafajnsdfadnfadfkoafasdklk@users.noreply.github.com>
2018-10-19 14:52:40 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ const fetchVideoInfo = require('youtube-info');
 
 const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
 
-const prefix = '6';
+const prefix = '7';
 
 client.on('ready', function() {
 
