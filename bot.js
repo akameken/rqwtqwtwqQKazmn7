2a6cb53f166220f17972c4fe6c88906d46da353b
dldadnasjndfsjafajnsdfadnfadfkoafasdklk <43988221+dldadnasjndfsjafajnsdfadnfadfkoafasdklk@users.noreply.github.com>
2018-10-19 10:39:05 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ const fetchVideoInfo = require('youtube-info');
 
 const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
 
-const prefix = '2';
+const prefix = '3';
 
 client.on('ready', function() {
 
